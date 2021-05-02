eac49177b1ab9373251309015997bc09dbd210df
Marc Anton Dahmen <mail@marcdahmen.de>
2021-05-02 17:22:53 +0200

    Update header.php

--- elements/header.php
+++ elements/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>@{ title }</title>
+	<title>@{ sitename } / @{ title }</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.2/css/bulma.min.css">
 	<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 </head>
