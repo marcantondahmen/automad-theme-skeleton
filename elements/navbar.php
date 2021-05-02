5598d6e8555220d876f467bb74e2f34cfc81183a
Marc Anton Dahmen <mail@marcdahmen.de>
2021-05-02 17:42:34 +0200

    Update navbar.php

--- elements/navbar.php
+++ elements/navbar.php
@@ -1,9 +1,9 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
 		<div class="navbar-item">
-			<a class="button is-white is-medium" href="/">
+			<a class="button is-white is-large" href="/">
 				<span class="icon">
-					<i class="fas fa-home"></i>
+					<i class="fas fa-circle has-text-grey-darker"></i>
 				</span>
 			</a>
 		</div>
